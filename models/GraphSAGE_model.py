c2ff03e902d4bfb417f9c72e7bd56adc32cb95c9
Ronald Albert <ronaldalbert1609@gmail.com>
2023-06-10 18:56:08 -0300

    Implemented GIN and GraphSAGE

---
+++ models/GraphSAGE_model.py
@@ -0,0 +1,84 @@
+# # Description: GraphSAGE model for node classification with PyTorch Geometric
+# Author: Ronald Albert
+# Last Modified: June 2023
+
+import torch
+import torch.nn.functional as F
+from torch.nn import BatchNorm1d, LogSoftmax
+from torch_geometric.nn import SAGEConv
+
+# ------------------------------------------------------------------------------
+# GraphSAGE model for node classification
+# ------------------------------------------------------------------------------
+# Parameters:
+# input_dim: dimension of the input features
+# hidden_dim: dimension of the hidden layers
+# output_dim: dimension of the output features
+# num_layers: number of layers
+# dropout: dropout probability
+# return_embeds: if True, the model returns the embeddings instead of the logits
+# ------------------------------------------------------------------------------
+# return: The GraphSAGE model
+# ------------------------------------------------------------------------------
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers,
+                 dropout, return_embeds=False):
+        super(GraphSAGE, self).__init__()
+
+        self.convs = None
+        self.bns = None
+        self.softmax = None
+
+        def get_in_channels(idx):
+            return hidden_dim if idx > 0 else input_dim
+
+        def get_out_channels(idx):
+            return hidden_dim if idx < num_layers - 1 else output_dim
+
+        self.convs = torch.nn.ModuleList([
+            SAGEConv(in_channels=get_in_channels(i), out_channels=get_out_channels(i))
+            for i in range(num_layers)
+        ])
+
+        self.bns = torch.nn.ModuleList([
+            BatchNorm1d(num_features=get_out_channels(i))
+            for i in range(num_layers - 1)
+        ])
+
+        self.softmax = LogSoftmax(dim=1)
+        self.dropout = dropout
+        self.return_embeds = return_embeds
+
+    # ------------------------------------------------------------------------------
+    # Reset the parameters of the model
+    # ------------------------------------------------------------------------------
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    # ------------------------------------------------------------------------------
+    # Forward pass of the model
+    # ------------------------------------------------------------------------------
+    # Parameters:
+    # batched_data: batched data from the dataloader
+    # ------------------------------------------------------------------------------
+    # return: the logits or the embeddings
+    # ------------------------------------------------------------------------------
+    def forward(self, batched_data):
+        x, adj_t = batched_data.x, torch.transpose(batched_data.adj_t,0,1)
+
+        out = None
+
+        for conv, bn in zip(self.convs[:-1], self.bns):
+            x = conv(x, adj_t)
+            x = bn(x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+
+        out = self.convs[-1](x, adj_t)
+        if not self.return_embeds:
+            out = self.softmax(out)
+
+        return out
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from models.GCN_model import GCN
+from models.GraphSAGE_model import GraphSAGE
+from models.GIN_model import GIN
 from config import device, results_dir,trained_models_dir
 from experiment import run_experiment
 from dataset import generate_dataset
@@ -6,6 +8,7 @@ from dataset import generate_dataset
 import random
 import pickle
 import torch
+from torch.nn import Sequential, Linear, ReLU, BatchNorm1d
 
 random.seed(7)
 
@@ -14,25 +17,46 @@ train_loader, valid_loader, test_loader, class_weights = generate_dataset(200, 0
 GCN_args = {
     'device': device,
     'num_layers': 3,
-    'hidden_dim': 256,
+    'hidden_dim': 64,
     'dropout': 0.5,
     'lr': 0.01,
     'epochs': 20,
 }
 
-num_features = 64
+GraphSAGE_args = {
+    'device': device,
+    'num_layers': 3,
+    'hidden_dim': 64,
+    'dropout': 0.5,
+    'lr': 0.01,
+    'epochs': 100,
+}
+
+GIN_args = {
+    'device': device,
+    'num_layers': 5,
+    'hidden_dim': 64,
+    'dropout': 0.5,
+    'lr': 0.01,
+    'epochs': 20,
+    'mlp_hidden_dim': 64,
+    'mlp': lambda input_dim, hidden_dim, output_dim: Sequential(Linear(input_dim, hidden_dim), BatchNorm1d(hidden_dim), ReLU(), Linear(hidden_dim, output_dim), ReLU())
+}
+num_features = 1
 num_labels = 2
 
-model = GCN(num_features, GCN_args['hidden_dim'],
-            num_labels, GCN_args['num_layers'],
-            GCN_args['dropout']).to(device)
+model = GIN(num_features, 
+            GIN_args['mlp'], GIN_args['mlp_hidden_dim'], 
+            GIN_args['hidden_dim'],
+            num_labels, GIN_args['num_layers'],
+            GIN_args['dropout']).to(device)
 
-GCN_results, GCN_model = run_experiment(model, GCN_args, train_loader, valid_loader, test_loader, class_weights)
+GIN_results, GIN_model = run_experiment(model, GIN_args, train_loader, valid_loader, test_loader, class_weights)
 
-GCN_results_filename = 'GCN_results.pickle'
-GCN_model_filename = 'GCN_model.pt'
+GIN_results_filename = 'GIN_results.pickle'
+GIN_model_filename = 'GIN_model.pt'
 
-with open(results_dir+GCN_results_filename, 'wb') as file:
-    pickle.dump(GCN_results, file, protocol=pickle.HIGHEST_PROTOCOL)
+with open(results_dir+GIN_results_filename, 'wb') as file:
+    pickle.dump(GIN_results, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-torch.save(GCN_model.state_dict(), trained_models_dir+GCN_model_filename)
+torch.save(GIN_model.state_dict(), trained_models_dir+GIN_model_filename)
